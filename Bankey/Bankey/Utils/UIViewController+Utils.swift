f67e9f0bb6c32b23e4333e3dd88cf0b8b703bde4
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-10-06 15:20:12 +0100

    changed deprecated code to new API in Utils

--- Bankey/Bankey/Utils/UIViewController+Utils.swift
+++ Bankey/Bankey/Utils/UIViewController+Utils.swift
@@ -9,12 +9,10 @@ import UIKit
 
 extension UIViewController {
     func setStatusBar() {
-        let statusBarSize = UIApplication.shared.statusBarFrame.size //deprecated
-        let frame = CGRect(origin: .zero, size: statusBarSize)
-        let statusbarView = UIView(frame: frame)
-        
-        statusbarView.backgroundColor = appColor
-        view.addSubview(statusbarView)
+        let navBarAppearance = UINavigationBarAppearance()
+        navBarAppearance.configureWithTransparentBackground() //hides nav bar line
+        UINavigationBar.appearance().standardAppearance = navBarAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = navBarAppearance
     }
     
     func setTabBarImage(imageName: String, title: String) {
