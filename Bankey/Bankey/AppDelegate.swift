8d482ef016c93eff47618910e52d13ab7a6630fb
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-10-12 11:19:40 +0100

    test: CurrencyFormatter unit tests

--- Bankey/Bankey/AppDelegate.swift
+++ Bankey/Bankey/AppDelegate.swift
@@ -16,7 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     let loginViewController = LoginViewController()
     let onboardingContainerViewController = OnboardingContainerViewController()
-    let dummyViewController = DummyViewController()
     let mainViewController = MainViewController()
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
@@ -26,16 +25,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window?.backgroundColor = .systemBackground
         
         loginViewController.delegate = self
-        
         onboardingContainerViewController.delegate = self
         
-        dummyViewController.logoutDelegate = self
+        let vc = mainViewController
+        vc.setStatusBar()
         
-        //window?.rootViewController = loginViewController
-        //window?.rootViewController = mainViewController
-        window?.rootViewController = AccountSummaryViewController()
-        //window?.rootViewController = onboardingContainerViewController
-        mainViewController.selectedIndex = 0
+        UINavigationBar.appearance().isTranslucent = false
+        UINavigationBar.appearance().backgroundColor = appColor
+        
+        window?.rootViewController = vc
         
         return true
     }
@@ -59,7 +57,7 @@ extension AppDelegate {
 extension AppDelegate: LoginViewControllerDelegate {
     func didLogin() {
         if LocalState.hasOnboarded {
-            setRootViewController(dummyViewController)
+            setRootViewController(mainViewController)
         } else {
             setRootViewController(onboardingContainerViewController)
         }
@@ -69,7 +67,7 @@ extension AppDelegate: LoginViewControllerDelegate {
 extension AppDelegate: OnboardingContainerViewControllerDelegate {
     func didFinishOnboarding() {
         LocalState.hasOnboarded = true
-        setRootViewController(dummyViewController)
+        setRootViewController(mainViewController)
     }
 }
 
---
+++ Bankey/BankeyUnitTests/CurrencyFormatterTests.swift
@@ -0,0 +1,48 @@
+//
+//  CurrencyFormatterTests.swift
+//  BankeyUnitTests
+//
+//  Created by Ryan Gallagher on 12/10/2023.
+//
+
+import Foundation
+
+import XCTest
+
+@testable import Bankey
+
+class CurrencyFormatterTests: XCTestCase {
+    var formatter: CurrencyFormatter!
+    
+    override func setUp() {
+        super.setUp()
+        formatter = CurrencyFormatter()
+    }
+    
+    func testBreakDollarsIntoCents() throws {
+        let result = formatter.breakIntoDollarsAndCents(929466.23)
+        XCTAssertEqual(result.0, "929,466")
+        XCTAssertEqual(result.1, "23")
+    }
+    
+    // Challenge: You write
+    func testDollarsFormatted() throws {
+        let result = formatter.dollarsFormatted(929466.23)
+        XCTAssertEqual(result, "$929,466.23")
+    }
+
+    // Challenge: You write
+    func testZeroDollarsFormatted() throws {
+        let result = formatter.dollarsFormatted(0.00)
+        XCTAssertEqual(result, "$0.00")
+    }
+    
+    // no hard-coded dollar
+    func testDollarsFormattedWithCurrencySymbol() {
+        let locale = Locale.current
+        let currencySymbol = locale.currencySymbol!
+        
+        let result = formatter.dollarsFormatted(929466.23)
+        XCTAssertEqual(result, "\(currencySymbol)929,466.23")
+    }
+}
