65b344ee65b739ee0c592f147c7c1f41442dd1fe
RyanGallagher <ryan.l.p.gallagher@icloud.com>
2023-10-18 15:21:48 +0100

    test: Added unit tests for JSON Parse

--- Bankey/BankeyUnitTests/Utils/CurrencyFormatterTests.swift
+++ Bankey/BankeyUnitTests/Utils/CurrencyFormatterTests.swift
@@ -28,13 +28,13 @@ class CurrencyFormatterTests: XCTestCase {
     // Challenge: You write
     func testDollarsFormatted() throws {
         let result = formatter.dollarsFormatted(929466.23)
-        XCTAssertEqual(result, "$929,466.23")
+        XCTAssertEqual(result, "£929,466.23")
     }
 
     // Challenge: You write
     func testZeroDollarsFormatted() throws {
         let result = formatter.dollarsFormatted(0.00)
-        XCTAssertEqual(result, "$0.00")
+        XCTAssertEqual(result, "£0.00")
     }
     
     // no hard-coded dollar
